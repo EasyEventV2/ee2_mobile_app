b38ee6f466a0aa881f4d405e8fb35e9c33985f62
spquyt <spquyt@gmail.com>
2019-10-22 22:18:59 +0700

    feat: set up absolute import

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    'module:react-native-dotenv',
+  ],
+  plugins: [
+    ['module-resolver', {
+      root: ['./src'],
+      alias: {
+        test: './test',
+        underscore: 'lodash',
+      },
+    }],
+  ],
 };
--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+import App from './src/components/App';
+
+// console.log('Yeaaaaahhhh');
+// import { appName } from './app.json';
+
+AppRegistry.registerComponent('ee2_mobile_app', () => App);
