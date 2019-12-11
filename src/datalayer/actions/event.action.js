ec65e6a158de6ab23ae9a7798edb74f83a5b951e
spquyt <spquyt@gmail.com>
2019-12-11 22:15:52 +0700

    fix: change param's name in api loadEventsList endpoint

--- src/datalayer/actions/event.action.js
+++ src/datalayer/actions/event.action.js
@@ -7,7 +7,7 @@ export const changeToLoginComponent = () => ({
 
 export const loadEventsListDispatch = (uid, page) => ({
   type: eventAction.LOAD_EVENTS_LIST,
-  promise: get(`/users/${uid}/events?page=${page}`, {}, true),
+  promise: get(`/users/${uid}/events?p=${page}`, {}, true),
 });
 
 export const loadEventDetailDispatch = (eventId) => ({
